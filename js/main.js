779f6ba2b5f82f5d681e86ad6e53aa63bba6774a
xPickeringx <xpickeringx11@gmail.com>
2025-02-24 10:45:03 -0600

    Agregando Bootstrap 5.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+// Obtener el elemento <canvas> del documento HTML
 var canvas = document.getElementById("canvas");
+
+// Obtener el contexto 2D del canvas para poder dibujar
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+// Establecer el color de relleno en verde
+ctx.fillStyle = "rgba(27, 209, 182, 0.83)";
+
+// Dibujar un rectángulo relleno en la posición (10,10) con un tamaño de 100x100 píxeles
+ctx.fillRect(10, 10, 500, 500);
